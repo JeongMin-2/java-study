59d8e7e42ee63185ed6d91661c80fc7f639a325b
JeongMin-2 <lisalee5@naver.com>
2023-03-30 17:53:46 +0900

    hi

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,8 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("hello world");
+        System.out.println("hello world");
     }
 
 
