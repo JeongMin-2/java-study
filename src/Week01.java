d15716042fd7b91ddfcea73db9575dd25ce41164
JeongMin-2 <lisalee5@naver.com>
2023-04-01 03:16:28 +0900

    message

--- src/Week01.java
+++ src/Week01.java
@@ -3,18 +3,15 @@ import java.util.Scanner;
 public class Week01 {
 
     public static void main(String[] args) {
-        Scanner s= new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
 
         int a = s.nextInt();
         int b = s.nextInt();
 
-        System.out.println(a+b);
-        System.out.println(a-b);
-        System.out.println(a*b);
-        System.out.println(a/b);
-        System.out.println(a%b);
-
-
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
     }
-
 }
